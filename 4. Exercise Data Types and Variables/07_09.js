184e512837163bb5f986b1aa79a3d70afc0009a4
Nuray Hakkieva-Ferhad <105154010+teacherHakkieva@users.noreply.github.com>
2022-07-05 17:53:37 +0300

    Lower Or Upper
    Add lowerOrUpper.js

---
+++ 4. Exercise Data Types and Variables/07_09.js
@@ -0,0 +1,10 @@
+function lowerOrUpper(params) {
+    let params1 = params.toLowerCase()
+    if (params === params1) {
+        console.log('lower-case');
+    } else {
+        console.log('upper-case');
+    }
+}
+lowerOrUpper('L');
+lowerOrUpper('f');
