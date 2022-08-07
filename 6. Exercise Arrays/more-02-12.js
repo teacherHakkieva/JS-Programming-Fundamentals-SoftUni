ac91c78da355aa8ede91777ec4978515737c6858
Nuray Hakkieva-Ferhad <105154010+teacherHakkieva@users.noreply.github.com>
2022-08-07 14:32:20 +0300

    addAndRemove
    Add addAndRemove.js

---
+++ 6. Exercise Arrays/more-02-12.js
@@ -0,0 +1,16 @@
+function addAndRemove(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === 'remove') {
+            result.pop();
+        } else if (arr[i] === 'add') {
+            result.push(i + 1);
+        }
+    }
+
+    if (result.length === 0) {
+        console.log('Empty');
+    } else {
+        console.log(result.join(' '));
+    }
+}
