c51646e8b48fb908fe83e269601ee2340356777f
Nuray Hakkieva-Ferhad <105154010+teacherHakkieva@users.noreply.github.com>
2022-09-04 23:37:49 +0300

    perfectNumber
    edit perfectNumber.js

--- 8. Exercise Functions/08_15.js
+++ 8. Exercise Functions/08_15.js
@@ -1,4 +1,21 @@
-function perfectNumber (params) {
+function perfectNumber (number) {
+    let perfectOrNo = getSum();
+    console.log(perfectOrNo);
+
+    function getSum() {
+        let divisorsSum = 0;
+        for (let i = 1; i <= number; i++) {
+            if (number % i === 0) {
+                divisorsSum += i;
+            }
+        }
+
+        if ((divisorsSum / 2) === number) {
+            return `We have a perfect number!`;
+        }
+
+        return 'It\'\s not so perfect.';
+    }
     
 }
 perfectNumber(6);
