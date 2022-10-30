b784233eb8b3051430311caf6380789402fd22ab
Nuray Hakkieva-Ferhad <105154010+teacherHakkieva@users.noreply.github.com>
2022-10-30 07:23:39 +0200

    classSong
    Add classSong.js

---
+++ 13. Objects and Classes/06_21.js
@@ -0,0 +1,29 @@
+function solve(input) {
+    class Song {
+        constructor(typeList, name, time) {
+            this.typeList = typeList;
+            this.name = name;
+            this.time = time;
+        }
+    }
+    let n = input.shift();
+    let songs = [];
+    for (let i = 0; i < n; i++) {
+        let currentInput = input.shift();
+        let [typeList, name, time] = currentInput.split('_');
+
+        let song = new Song(typeList, name, time);
+        songs.push(song)
+    }
+    let lastEl = input.pop();
+    for (const song of songs) {
+        if (song.typeList == lastEl || lastEl=='all')
+            console.log(song.name);
+    }
+}
+solve([3,
+    'favourite_DownTown_3:14',
+    'favourite_Kiss_4:16',
+    'favourite_Smooth Criminal_4:01',
+    'favourite']
+)
